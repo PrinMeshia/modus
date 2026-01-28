2b7db1bd9933bcd8008fca530a3d9f33c047e9ac
GitHub Action <action@github.com>
2026-01-28 14:20:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/prinmeshia/modus/MyBundle.kt
+++ src/main/kotlin/com/github/prinmeshia/modus/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.prinmeshia.modus
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/prinmeshia/modus/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/prinmeshia/modus/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.prinmeshia.modus.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
